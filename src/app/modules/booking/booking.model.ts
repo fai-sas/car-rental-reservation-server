2de6dddbbda01902b942f615605fecdd9decc6e3
fai-sas <craftedbyfaisal@gmail.com>
2024-06-11 21:43:47 +0600

    booking model added

---
+++ src/app/modules/booking/booking.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose'
+import { TBooking } from './booking.interface'
+
+const bookingSchema = new Schema<TBooking>({
+  date: {
+    type: Date,
+    required: [true, 'Booking date is required'],
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User ID is required'],
+  },
+  car: {
+    type: Schema.Types.ObjectId,
+    ref: 'Car',
+    required: [true, 'Car ID is required'],
+  },
+  startTime: {
+    type: String,
+    required: [true, 'Start time is required'],
+  },
+  endTime: {
+    type: String,
+    required: [true, 'End time is required'],
+  },
+  totalCost: {
+    type: Number,
+    default: 0,
+  },
+  isBooked: {
+    type: String,
+    enum: ['unconfirmed', 'confirmed'],
+    default: 'unconfirmed',
+  },
+})
+
+export const Booking = model<TBooking>('Booking', bookingSchema)

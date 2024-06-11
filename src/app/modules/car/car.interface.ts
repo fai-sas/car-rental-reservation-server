c7b6bf709d27f51f3482332172f1f403f9106b79
fai-sas <craftedbyfaisal@gmail.com>
2024-06-11 20:38:35 +0600

    car interface added

---
+++ src/app/modules/car/car.interface.ts
@@ -0,0 +1,10 @@
+export interface Car {
+  name: string
+  description: string
+  color: string
+  isElectric: boolean
+  status: 'available' | 'unavailable'
+  features: string[]
+  pricePerHour: number
+  isDeleted: boolean
+}

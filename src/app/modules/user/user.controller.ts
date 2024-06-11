d125b6347d16c67f363dad43590425d1e3e6aed9
fai-sas <craftedbyfaisal@gmail.com>
2024-06-11 19:32:17 +0600

    user sign up controller added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,6 +1,18 @@
+import httpStatus from 'http-status'
 import catchAsync from '../../utils/catchAsync'
+import sendResponse from '../../utils/sendResponse'
+import { UserServices } from './user.service'
 
-const signUpUser = catchAsync(async (req, res) => {})
+const signUpUser = catchAsync(async (req, res) => {
+  const result = await UserServices.signUpUserIntoDb(req.body)
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User registered successfully',
+    data: result,
+  })
+})
 
 const signInUser = catchAsync(async (req, res) => {})
 

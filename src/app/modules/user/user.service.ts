cdd1ead7e1e055b1cf26c0e49b107856b7e0abab
fai-sas <craftedbyfaisal@gmail.com>
2024-06-11 19:46:34 +0600

    user sign up service added

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,6 +1,17 @@
 import { TUser } from './user.interface'
+import { User } from './user.model'
 
-const signUpUserIntoDb = async (payload: TUser) => {}
+const signUpUserIntoDb = async (payload: TUser) => {
+  const existingUser = await User.findOne({ email: payload.email })
+
+  if (existingUser) {
+    throw new Error('User with this email already exists')
+  }
+
+  const result = await User.create(payload)
+
+  return result
+}
 
 const signInUserIntoDb = async (payload: Partial<TUser>) => {}
 

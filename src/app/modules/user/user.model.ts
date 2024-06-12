9a892d265a1e45df4004ad75b292345a399630a2
fai-sas <craftedbyfaisal@gmail.com>
2024-06-12 21:52:39 +0600

    hashed password while signing up

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,7 @@
+import bcrypt from 'bcrypt'
 import { Schema, model } from 'mongoose'
 import { TUser } from './user.interface'
+import config from '../../config'
 
 const userSchema = new Schema<TUser>(
   {
@@ -15,7 +17,7 @@ const userSchema = new Schema<TUser>(
     password: {
       type: String,
       required: [true, 'Password is required'],
-      select: false,
+      select: 0,
     },
     role: {
       type: String,
@@ -38,5 +40,19 @@ const userSchema = new Schema<TUser>(
     timestamps: true,
   }
 )
+
+userSchema.pre('save', async function (next) {
+  const user = this
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  )
+  next()
+})
+
+userSchema.post('save', function (doc, next) {
+  doc.password = ''
+  next()
+})
 
 export const User = model<TUser>('User', userSchema)
